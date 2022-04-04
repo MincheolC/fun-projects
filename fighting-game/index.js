88810a4aabf8b490e81d7ca2b7a01a1d82701a51
MincheolCha <mc.cha@greenlabs.co.kr>
2022-04-04 18:25:14 +0900

    fighting-game: create player and enemy

--- fighting-game/index.js
+++ fighting-game/index.js
@@ -1,7 +1,67 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
 canvas.width = 1024;
 canvas.height = 576;
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const gravity = 0.2;
+
+// 영상 속에 작은 2차원 비트맵이나 애니매이션을 합셩하는 기술
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.position.x, this.position.y, 50, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+    
+    if ((this.position.y + this.height + this.velocity.y) >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity
+    }
+  }
+}
+
+const player = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+const enemy = new Sprite({
+  position: {
+    x: 400,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+// 멈추기 전까지 무한 루프
+function animate() {
+  window.requestAnimationFrame(animate);
+  ctx.fillStyle = 'black'
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+}
+
+animate();
