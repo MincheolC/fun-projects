964143be8888e63cd2fab0dfed1b6ab80962d118
MincheolCha <mc.cha@greenlabs.co.kr>
2022-04-07 23:16:17 +0900

    fighting-game: enemy health bar

--- fighting-game/index.js
+++ fighting-game/index.js
@@ -29,6 +29,7 @@ class Sprite {
     };
     this.color = color;
     this.isAttacking;
+    this.health = 100
   }
 
   draw() {
@@ -164,6 +165,8 @@ function animate() {
   ) {
     player.isAttacking = false;
     console.log("hit!");
+    enemy.health -= 20
+    document.querySelector('#enemyHealth').style.width = enemy.health
   }
 
   if (
